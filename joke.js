cc7457f666e1ac76f72c4eed45f35fe3f039787e
Josh Hamilton <nearbycoder@gmail.com>
2021-12-10 17:29:29 -0600

    first commit

---
+++ joke.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch';
+import { App } from '@slack/bolt';
+
+export const run = async (event, context) => {
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
+
+  try {
+    const response = await fetch('https://icanhazdadjoke.com/slack');
+
+    const joke = await response.json();
+
+    app.client.chat.postMessage({
+      channel: 'dadjokes',
+      text: joke.attachments[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
